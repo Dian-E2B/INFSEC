d49200bb012182f2adb649f3c0bc885d4df71b8c
Dian-E2B <d.emuy.464149@umindanao.edu.ph>
2021-03-16 09:20:31 +0800

    unfix- siebar tables

--- z_otherUI/sidebartables.php
+++ z_otherUI/sidebartables.php
@@ -1,16 +1,38 @@
 <!DOCTYPE HTML>
 
-<div class="sidebar" data-color="blue">
+<div class="sidebar" data-color="purple">
 
 <!--   you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple" -->
 
 
 <div class="sidebar-wrapper">
-    <div class="logo">
-        <a class="simple-text">
-            <?php echo $_SESSION['username'];  ?>
-        </a>
+    <div class="" style="margin-bottom: -10px;">
+        <ul class="nav nav-bar">
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle"  data-toggle="dropdown" >
+            <p>
+            
+            <div style="font-size: 20px; text-align: center; font-weight: bold; text-transform: uppercase;"><?php echo $_SESSION['username'];  ?></div>
+             
+
+        </p>
+       </a>
+       
+         <ul class="dropdown-menu">
+                                    <li><a href="./add_productform.php"><i style="font-size:16px;"class="fas fa-cubes"></i>&nbsp;Add Product</a></li>
+                                    <li><a href="./add_supplierform.php"><i class="fas fa-parachute-box"></i>&nbsp;Add Supplier</a></li>
+                                    <li><a href="./table1.php"><i style="font-size:20px;" class="fad fa-window-close"></i>&nbsp;Inactive Products</a></li>
+                                    <!-- <li><a href="#">Temp</a></li>
+                                    <li><a href="#">Temp</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Temp</a></li> -->
+                               
+                                 
+        </ul>
+        </li>
+        </ul>
     </div>
+    <hr>
 
     <ul class="nav">
         <li>
@@ -54,3 +76,24 @@
     </ul>
 </div>
 </div>
+<script src="assets/js/jquery-3.5.1.min.js" type="text/javascript"></script>
+<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+<!--  Charts Plugin -->
+<script src="assets/js/chartist.min.js"></script>
+<!--  Notifications Plugin    -->
+<script src="assets/js/bootstrap-notify.js"></script>
+
+<!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+<script src="assets/js/light-bootstrap-dashboard.js"></script>
+<!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+<script src="assets/js/demo.js"></script>
+<script>
+
+var input = document.getElementById("myInput");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("myBtn").click();
+  }
+});
+</script>
